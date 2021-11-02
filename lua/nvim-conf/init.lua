e5e1fd077b0ac8a5348f4fd1d5a38e520ab828ba
SergioRibera <sergioalejandroriberacosta@gmail.com>
2021-11-02 18:27:42 -0400

    Set new mode to load data and add get/set functions into data for the on load callback

--- lua/nvim-conf/init.lua
+++ lua/nvim-conf/init.lua
@@ -5,7 +5,7 @@ local M = {}
 
 M.settings = {
     conf_file = vim.fn.stdpath('config') .. "/settings.conf",
-    load_on_start = true,
+    load_event = "load", -- posibilities: load, setup, none
     on_load = function (_)
         print("Data Loaded success")
     end,
@@ -28,11 +28,16 @@ function M.set_value (name, value)
     M.save()
 end
 
+
 function M.load()
     local data_loaded = core.load_data(M.settings.conf_file, true)
     if data_loaded ~= {} then
         M.data_loaded = data_loaded
     end
+    -- Set function to get/set value with data and without use other import
+    M.data_loaded.get_value = M.get_value
+    M.data_loaded.set_value = M.set_value
+    -- callback to on load
     M.settings.on_load(M.data_loaded)
     M.is_loaded = true
 end
@@ -46,10 +51,12 @@ function M.setup(opts)
     -- load user options with default options
     M.settings = vim.tbl_extend('force', M.settings, opts)
 
-    if M.settings.load_on_start == true then
+    if M.settings.load_event == "load" then
         au.group('ConfigGroup', function (_)
             au({ 'VimEnter', 'BufReadPre', 'FileReadPre' }, { '*', M.load })
         end)
+    elseif M.settings.load_event == "setup" then
+        M.load()
     end
 end
 
